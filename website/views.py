78a09bf5b3f4b719653f228cbccdf4079036c0b4
andrewww05 <118112331+andrewww05@users.noreply.github.com>
2023-11-27 02:06:31 +0200

    v0.5.0 [by Max]

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, jsonify, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Like
 from . import db
 
 views = Blueprint("views", __name__)
@@ -66,19 +66,20 @@ def posts(username):
 def create_comment(post_id):
     text = request.form.get('text')
 
-    if not text:
+    if not text.strip():
         flash('Comment cannot be empty.', category='error')
     else:
-     post = Post.query.filter_by(id = post_id)
-    if post:
-     comment = Comment(text=text, author=current_user.id, post_id=post_id)
-     db.session.add(comment)
-     db.session.commit()
-    else:
-     flash('Post does not exist', category='error')
+        post = Post.query.filter_by(id=post_id).first()
 
+        if post:
+            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist', category='error')
 
     return redirect(url_for('views.home'))
+
 
 @views.route("/delete-comment/<comment_id>")
 @login_required
@@ -94,3 +95,22 @@ def delete_comment(comment_id):
       db.session.commit()
       
    return redirect(url_for('views.home'))
+
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        return jsonify({'error', 'Post does not exist'},400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author,post.likes)})
